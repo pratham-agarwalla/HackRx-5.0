861f18e39ffd91acf2abd7d8f94471e8013d3a8f
PRATHAM AGARWALLA <ps2285@srmist.edu.in>
2024-10-08 00:24:09 +0530

    Update lvl2.py

--- lvl2.py
+++ lvl2.py
@@ -27,7 +27,7 @@ FR_KEY = os.getenv('AZURE_KEY')
 
 # Initialize the Document Analysis Client
 document_analysis_client = DocumentAnalysisClient(
-    endpoint=FR_ENDPOINT, credential=AzureKeyCredential(FR_KEY)
+    endpoint=str(FR_ENDPOINT), credential=AzureKeyCredential(str(FR_KEY))
 )
 
 # Function to extract text from PDF
